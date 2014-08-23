b32536528f1552a16f99599fb12840706791ecea
Alexandre <alexandre@kohen.fr>
2014-08-23 14:17:03 +0200

    With States

--- public/game/main.js
+++ public/game/main.js
@@ -1,54 +1,50 @@
-window.onload = function() {
+(function() {
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-        var player, blue, cursors;
+	BasicGame = {
+    	score: 0
+	};
 
-        function preload () {
+	BasicGame.DefaultState = function (game) {
+	};
 
-            game.load.image('red', '../asset/redCube.png');
-            game.load.image('blue', '../asset/blueCube.png');
-            game.load.image('green', '../asset/greenCube.png');
-            game.load.image('logo', '../asset/phaser.png');
+	BasicGame.DefaultState.prototype = {
+		preload: function() {
+			game.load.image('red', '../asset/redCube.png');
+			game.load.image('blue', '../asset/blueCube.png');
+			game.load.image('green', '../asset/greenCube.png');
+			game.load.image('logo', '../asset/phaser.png');
+		},
 
-        }
+		create: function() {
+			game.world.setBounds(0, 0, 1920, 1920);
+			game.physics.startSystem(Phaser.Physics.ARCADE);
+			var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+			logo.anchor.setTo(0.5, 0.5);
 
-        function create () {
-        	game.world.setBounds(0, 0, 1920, 1920);
-            game.physics.startSystem(Phaser.Physics.ARCADE);
-            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
+			this.blue = game.add.sprite(game.world.centerX, game.world.centerY-200, 'blue');
+			this.game.physics.enable(this.blue, Phaser.Physics.ARCADE);
+			this.blue.body.immovable = true;
 
-            blue = game.add.sprite(game.world.centerX, game.world.centerY-200, 'blue');
-           	this.game.physics.enable(blue, Phaser.Physics.ARCADE);
-			blue.body.immovable = true;
+			this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'red');
+			this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
 
-			player = game.add.sprite(game.world.centerX, game.world.centerY, 'red');
-			this.game.physics.enable(player, Phaser.Physics.ARCADE);
-			
-	        var groundBlock = this.game.add.sprite(1000, 1000, 'green');
-	        this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
-	        
-			cursors = game.input.keyboard.createCursorKeys();
-			game.camera.follow(player);
-        }
+			var groundBlock = this.game.add.sprite(150, 100, 'green');
+			this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
+			groundBlock.fixedToCamera = true;
 
+			this.cursors = new Input(game.input.keyboard.createCursorKeys());
+			game.camera.follow(this.player);
+		},
 
-        function update () {
-        	player.body.velocity.x = 0;
-        	player.body.velocity.y = 0;
+		update: function() {
+			this.cursors.updateVelocity(this.player.body);
+			game.physics.arcade.collide(this.player, this.blue);
+		}
+	}
 
-			if (cursors.up.isDown) {
-				player.body.velocity.y = -300;
-			} else if (cursors.down.isDown) {
-				player.body.velocity.y = 300;
-			}
+	var state1 = new BasicGame.DefaultState();
 
-			if (cursors.left.isDown) {
-				player.body.velocity.x = -300;
-			} else if (cursors.right.isDown) {
-			player.body.velocity.x = 300;
-			}
-			game.physics.arcade.collide(player,blue);		
-        }
-
-    };
+	game.state.add('state1', state1);
+	game.state.start('state1');
+})();
